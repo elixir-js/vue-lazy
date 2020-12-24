18d2ac2026420d6054ddb56d03acbc7644c00bd2
Jovid <voidjmp@mail.ru>
2020-12-24 09:34:46 +0500

    :hammer: feat version

--- src/lazyloading.js
+++ src/lazyloading.js
@@ -1,46 +1,48 @@
 export default {
   inserted: (el) => {
-    const className = el.getAttribute('class')
-    const src = el.getAttribute('src')
-    el.setAttribute('class', 's-image')
-    el.setAttribute('style', 'height: 170px;')
-    el.setAttribute('src', require('./assets/img/img_skeleton.png'))
+    const style = el.dataset.style
+    const img = el.dataset.image
+    const className = el.getAttribute('class');
+    const src = el.getAttribute('src');
+    el.setAttribute('class', 's-image');
+    el.setAttribute('style', !style ? 'height: 170px;' : style);
+    el.setAttribute('src', !img ? require('./assets/img/img_skeleton.png') : img);
 
     const loadImage = () => {
-      el.src = src
-    }
+      el.src = src;
+    };
 
     const callback = (entries, observer) => {
-      entries.forEach((entry) => {
+      entries.forEach(entry => {
         if (entry.isIntersecting) {
-          loadImage()
-          observer.unobserve(el)
+          loadImage();
+          observer.unobserve(el);
         }
-      })
-    }
+      });
+    };
 
     const lazyImage = () => {
       const options = {
         root: null,
-        threshold: 0.1,
-      }
-      const observer = new IntersectionObserver(callback, options)
-      observer.observe(el)
-    }
+        threshold: 1
+      };
+      const observer = new IntersectionObserver(callback, options);
+      observer.observe(el);
+    };
 
     el.onload = () => {
-      if (el.getAttribute('src') !== '../assets/img/img_skeleton.png') {
-        el.setAttribute('style', '')
-        el.setAttribute('class', className)
+      if (el.getAttribute('src') !== 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAALwAAADGAQMAAABSPnhPAAAAA1BMVEXm5uSlVuPDAAAAG0lEQVR4Ae3BMQEAAADCIPunNsN+YAAAAABAdBNWAAHMZ56dAAAAAElFTkSuQmCC') {
+        el.setAttribute('style', '');
+        el.setAttribute('class', className);
       }
-    }
+    };
 
     el.onerror = () => {
-      el.setAttribute('src', require('./assets/img/photo.png'))
-      el.setAttribute('style', '')
-      el.setAttribute('class', className)
-    }
+      el.setAttribute('src', require('./assets/img/photo.png'));
+      el.setAttribute('style', '');
+      el.setAttribute('class', className);
+    };
 
-    lazyImage()
+    lazyImage();
   },
 }
