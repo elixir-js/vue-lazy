4a39f9e933fee9685d168ac19d17ccbb4cd57f40
fayzzz <mustafo.fayz@yandex.ru>
2021-01-04 18:47:07 +0500

    Initialized @prettier config for linting.

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    printWidth: 80,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    endOfLine: 'auto',
+    tabWidth: 4,
+    semi: true,
+};
