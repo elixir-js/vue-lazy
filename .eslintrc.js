5b90396fe7dbc4f94430c0effe5d66445ab62b98
fayzzz <mustafo.fayz@yandex.ru>
2021-01-05 20:44:35 +0500

    Added jest for testing our project. Has added several tests for the beginning.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,17 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        'jest/globals': true,
     },
     extends: ['airbnb-base'],
     parserOptions: {
         ecmaVersion: 12,
         sourceType: 'module',
     },
+    plugins: ['jest'],
     rules: {
         indent: ['error', 4],
         'operator-linebreak': [2, 'after', { overrides: { '?': 'after', ':': 'after' } }],
+        linebreak: 'none',
     },
 };
---
+++ src/__tests__/app.test.js
@@ -0,0 +1,7 @@
+test('Check Validation of random function', () => {
+    expect(1).toBe(1);
+});
+
+test('Check validation of prime function', () => {
+    expect(2).toBe(2);
+});
